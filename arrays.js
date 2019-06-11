06cf0f3d884211f4eebd80d3a9502a9c8a960407
Alethia Quintero <alethiaq22@gmail.com>
2019-06-11 18:22:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, foo){
   return chocolateBars;
 }
 
-function accessElementInArray(chocolateBars, 2){
-  
+function accessElementInArray(chocolateBars, index){
+  return chocolateBars["index"];
 }
