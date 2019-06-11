cba0ceaf8521e672f89133eea45cc289f97f2e59
Alethia Quintero <alethiaq22@gmail.com>
2019-06-11 18:33:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,10 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars;
 }
 
-
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars = chocolateBars.slice(0,chocolateBars.length -1)
+  return chocolateBars;
+}
 
 
 
