14c258b3e375efb2dce49568f41da56554ca2087
Alethia Quintero <alethiaq22@gmail.com>
2019-06-11 18:18:30 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(chocolateBars, foo) { 
+   chocolateBars =  ["foo",...chocolateBars];
+   return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars,foo) {
+  chocolateBars.unshift("foo");
+  return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, foo) {
+  chocolateBars = [...chocolateBars,"foo"];
+  return chocolateBars;
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, foo){
+  chocolateBars.push("foo");
+  return chocolateBars;
+}
+
+function accessElementInArray()
